47fd75c3dcf07e61bba3f3c4d113c91d43a1e250
sophi <ss@covizmo.com>
2021-09-05 20:39:46 +0300

    Add project files.

---
+++ task01_MatrixSum/task01_MatrixSum/Matrix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace task01_MatrixSum
+{
+    public class Matrix
+    {
+        public uint Rows { get; set; }
+        public uint Columns { get; set; }
+
+        public int[,] MatrixArray { get; set; }
+
+        public int this[long i, long j]
+        {
+            get
+            {
+                if (i < 0 || i >= Rows)
+                    throw new IndexOutOfRangeException("Row is out of range");
+                if (j < 0 || j >= Columns)
+                    throw new IndexOutOfRangeException("Column is out of range");
+                return MatrixArray[i, j];
+            }
+            set
+            {
+                if (i < 0 || i >= Rows)
+                    throw new IndexOutOfRangeException("Row is out of range");
+                if (j < 0 || j >= Columns)
+                    throw new IndexOutOfRangeException("Column is out of range");
+                MatrixArray[i, j] = value;
+            }
+        }
+
+        public Matrix(uint rows, uint columns, bool rand = true)
+        {
+            Rows = rows;
+            Columns = columns;
+            MatrixArray = new int[Rows, Columns];
+            if (rand)
+            {
+                GenerateRandomMatrix(MatrixArray, Rows, Columns);
+            }
+        }
+
+        private static void GenerateRandomMatrix(int[,] matrixArray, uint rows, uint columns)
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrixArray[i, j] = randNum.Next(1000);
+                }
+            }
+        }
+
+        public static Matrix AddMatrixLoops(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+                throw new ArgumentException("Matrix must have same dimensions");
+
+            var resultMatrix = new Matrix(a.Rows, b.Columns, false);
+            for (int i = 0; i < resultMatrix.Rows; i++)
+            {
+                for (int j = 0; j < resultMatrix.Columns; j++)
+                {
+                    resultMatrix[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return resultMatrix;
+        }
+
+        public static Matrix AddMatrixThreads(Matrix a, Matrix b, uint amount)
+        {
+            Thread[] threads = new Thread[amount];
+            uint start = 0;
+            var step = (a.Columns * a.Rows) / (amount);
+            var end = step + 1;
+            var resultMatrix = new Matrix(a.Rows, b.Columns, false);
+            for (int i = 0; i < amount; i++)
+            {
+                var st = start;
+                var en = end;
+
+                threads[i] = new Thread(() =>
+                {
+                    for (var j = st; j <= en; j++)
+                    {
+                        resultMatrix[j / a.Columns, j % a.Columns] =
+                            a[j / a.Columns, j % a.Columns] + b[j / a.Columns, j % a.Columns];
+                    }
+                });
+
+                threads[i].Start();
+                // Thread.Sleep(2000);
+                start = end + 1;
+                end = i == amount - 2 ? a.Rows * b.Columns - 1 : end + step;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            return resultMatrix;
+        }
+
+        public override string ToString()
+        {
+            string toReturn = " ";
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                    toReturn += MatrixArray[i, j].ToString() + " ";
+                toReturn += "\n ";
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ task01_MatrixSum/task01_MatrixSum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace task01_MatrixSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            uint rows = 5;
+            uint columns = 11;
+            uint threds = 10;
+
+            var firstMatrix = new Matrix(rows, columns);
+            var secondMatrix = new Matrix(rows, columns);
+
+            Console.WriteLine($" First Matrix: \n{firstMatrix}\n");
+            Console.WriteLine($" Second Matrix: \n{secondMatrix}\n");
+
+            var res1 = Matrix.AddMatrixLoops(firstMatrix, secondMatrix);
+            var res2 = Matrix.AddMatrixThreads(firstMatrix, secondMatrix, threds);
+
+
+            Console.WriteLine($" AddMatrixLoop: \n{res1}\n");
+            Console.WriteLine($" AddMatrixThreads: \n{res2}\n");
+
+
+            rows = 10000;
+            columns = 10000;
+            firstMatrix = new Matrix(rows, columns);
+            secondMatrix = new Matrix(rows, columns);
+
+            Console.WriteLine($" rows: {rows}\n columns: {columns}");
+            var watch3 = System.Diagnostics.Stopwatch.StartNew();
+            var res3 = Matrix.AddMatrixLoops(firstMatrix, secondMatrix);
+            watch3.Stop();
+            Console.WriteLine($" One thread: {watch3.Elapsed}");
+            for (uint i = 2; i <= 10; i += 1)
+            {
+                watch3 = System.Diagnostics.Stopwatch.StartNew();
+                res3 = Matrix.AddMatrixThreads(firstMatrix, secondMatrix, i);
+                watch3.Stop();
+                Console.WriteLine($" {i} threads: {watch3.Elapsed}");
+
+            }
+            for (uint i = 100; i <= 1000; i += 100)
+            {
+                watch3 = System.Diagnostics.Stopwatch.StartNew();
+                res3 = Matrix.AddMatrixThreads(firstMatrix, secondMatrix, i);
+                watch3.Stop();
+                Console.WriteLine($" {i} threads : {watch3.Elapsed}");
+            }
+
+        }
+    }
+}
