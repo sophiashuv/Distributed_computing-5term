83da018318986934af393161fad60b6acc01df65
sophi <ss@covizmo.com>
2021-10-19 04:10:12 +0300

    add task4

---
+++ task04_ParalelForms/task04_ParalelForms/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace task04_ParalelForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonStart1 = new System.Windows.Forms.Button();
+            this.buttonStop1 = new System.Windows.Forms.Button();
+            this.buttonStart2 = new System.Windows.Forms.Button();
+            this.buttonStop2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonStart3 = new System.Windows.Forms.Button();
+            this.buttonStop3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.buttonStart4 = new System.Windows.Forms.Button();
+            this.buttonStop4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(16, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(448, 324);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Location = new System.Drawing.Point(492, 15);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(432, 324);
+            this.panel2.TabIndex = 1;
+            // 
+            // buttonStart1
+            // 
+            this.buttonStart1.Location = new System.Drawing.Point(16, 363);
+            this.buttonStart1.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart1.Name = "buttonStart1";
+            this.buttonStart1.Size = new System.Drawing.Size(100, 28);
+            this.buttonStart1.TabIndex = 2;
+            this.buttonStart1.Text = "Start";
+            this.buttonStart1.UseVisualStyleBackColor = true;
+            this.buttonStart1.Click += new System.EventHandler(this.buttonStart1_Click);
+            // 
+            // buttonStop1
+            // 
+            this.buttonStop1.Location = new System.Drawing.Point(364, 363);
+            this.buttonStop1.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop1.Name = "buttonStop1";
+            this.buttonStop1.Size = new System.Drawing.Size(100, 28);
+            this.buttonStop1.TabIndex = 3;
+            this.buttonStop1.Text = "Stop";
+            this.buttonStop1.UseVisualStyleBackColor = true;
+            this.buttonStop1.Click += new System.EventHandler(this.buttonStop1_Click);
+            // 
+            // buttonStart2
+            // 
+            this.buttonStart2.Location = new System.Drawing.Point(544, 363);
+            this.buttonStart2.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart2.Name = "buttonStart2";
+            this.buttonStart2.Size = new System.Drawing.Size(100, 28);
+            this.buttonStart2.TabIndex = 4;
+            this.buttonStart2.Text = "Start";
+            this.buttonStart2.UseVisualStyleBackColor = true;
+            this.buttonStart2.Click += new System.EventHandler(this.buttonStart2_Click);
+            // 
+            // buttonStop2
+            // 
+            this.buttonStop2.Location = new System.Drawing.Point(824, 363);
+            this.buttonStop2.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop2.Name = "buttonStop2";
+            this.buttonStop2.Size = new System.Drawing.Size(100, 28);
+            this.buttonStop2.TabIndex = 5;
+            this.buttonStop2.Text = "Stop";
+            this.buttonStop2.UseVisualStyleBackColor = true;
+            this.buttonStop2.Click += new System.EventHandler(this.buttonStop2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel3.Location = new System.Drawing.Point(13, 424);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(448, 324);
+            this.panel3.TabIndex = 6;
+            // 
+            // buttonStart3
+            // 
+            this.buttonStart3.Location = new System.Drawing.Point(13, 765);
+            this.buttonStart3.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart3.Name = "buttonStart3";
+            this.buttonStart3.Size = new System.Drawing.Size(100, 28);
+            this.buttonStart3.TabIndex = 7;
+            this.buttonStart3.Text = "Start";
+            this.buttonStart3.UseVisualStyleBackColor = true;
+            this.buttonStart3.Click += new System.EventHandler(this.buttonStart3_Click);
+            // 
+            // buttonStop3
+            // 
+            this.buttonStop3.Location = new System.Drawing.Point(364, 765);
+            this.buttonStop3.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop3.Name = "buttonStop3";
+            this.buttonStop3.Size = new System.Drawing.Size(100, 28);
+            this.buttonStop3.TabIndex = 8;
+            this.buttonStop3.Text = "Stop";
+            this.buttonStop3.UseVisualStyleBackColor = true;
+            this.buttonStop3.Click += new System.EventHandler(this.buttonStop3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel4.Location = new System.Drawing.Point(492, 424);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(432, 324);
+            this.panel4.TabIndex = 9;
+            // 
+            // buttonStart4
+            // 
+            this.buttonStart4.Location = new System.Drawing.Point(492, 765);
+            this.buttonStart4.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart4.Name = "buttonStart4";
+            this.buttonStart4.Size = new System.Drawing.Size(100, 28);
+            this.buttonStart4.TabIndex = 10;
+            this.buttonStart4.Text = "Start";
+            this.buttonStart4.UseVisualStyleBackColor = true;
+            this.buttonStart4.Click += new System.EventHandler(this.buttonStart4_Click);
+            // 
+            // buttonStop4
+            // 
+            this.buttonStop4.Location = new System.Drawing.Point(824, 765);
+            this.buttonStop4.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop4.Name = "buttonStop4";
+            this.buttonStop4.Size = new System.Drawing.Size(100, 28);
+            this.buttonStop4.TabIndex = 11;
+            this.buttonStop4.Text = "Stop";
+            this.buttonStop4.UseVisualStyleBackColor = true;
+            this.buttonStop4.Click += new System.EventHandler(this.buttonStop4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 806);
+            this.Controls.Add(this.buttonStop4);
+            this.Controls.Add(this.buttonStart4);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.buttonStop3);
+            this.Controls.Add(this.buttonStart3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.buttonStop2);
+            this.Controls.Add(this.buttonStart2);
+            this.Controls.Add(this.buttonStop1);
+            this.Controls.Add(this.buttonStart1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonStart1;
+        private System.Windows.Forms.Button buttonStop1;
+        private System.Windows.Forms.Button buttonStart2;
+        private System.Windows.Forms.Button buttonStop2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonStart3;
+        private System.Windows.Forms.Button buttonStop3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button buttonStart4;
+        private System.Windows.Forms.Button buttonStop4;
+    }
+}
+
---
+++ task04_ParalelForms/task04_ParalelForms/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace task04_ParalelForms
+{
+    public partial class Form1 : Form
+    {
+        private List<Thread> threads;
+        private List<bool> isRunning;
+
+        public Form1()
+        {
+            InitializeComponent();
+            threads = new List<Thread>();
+            isRunning = new List<bool>
+            {
+                true,
+                true,
+                true,
+                true
+            };
+            buttonStart1.Enabled = false;
+            buttonStart2.Enabled = false;
+            buttonStart3.Enabled = false;
+            buttonStart4.Enabled = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            threads.Add(new Thread(Panel1Draw));
+            threads.Add(new Thread(Panel2Draw));
+            threads.Add(new Thread(Panel3Draw));
+            threads.Add(new Thread(Panel4Draw));
+            threads.ForEach(p => p.Start());
+        }
+
+        public void Panel1Draw()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Bitmap btm = new Bitmap(panel1.Width, panel1.Height);
+            Graphics gImg = Graphics.FromImage(btm);
+            var alpha = 0.0;
+            var a = 70;
+            var speed = 20;
+            PointF[] rect;
+
+
+            PointF img = new PointF(0, 0);
+            var frameCount = 0;
+            SolidBrush color = new SolidBrush(Color.Blue);
+            while (true)
+            {
+                if (frameCount % speed == 0)
+                {
+                    color = new SolidBrush(Color.FromArgb(frameCount * 3 % 255, frameCount * 5 % 255, frameCount * 7 % 255));
+                    rect = new[] {
+                    new PointF((float)((-a) * Math.Cos(alpha) - (-a) * Math.Sin(alpha)) + panel4.Width / 2, (float)((-a) * Math.Cos(alpha) + (-a) * Math.Sin(alpha)) + panel4.Height / 2),
+                    new PointF((float)((-a) * Math.Cos(alpha) - a * Math.Sin(alpha)) + panel4.Width / 2, (float)(a * Math.Cos(alpha) + (-a) * Math.Sin(alpha)) +  panel4.Height / 2),
+                    new PointF((float)(a * Math.Cos(alpha) - a * Math.Sin(alpha)) + panel4.Width / 2, (float)(a * Math.Cos(alpha) + a * Math.Sin(alpha)) + panel4.Height / 2),
+                    new PointF((float)(a * Math.Cos(alpha) - (-a) * Math.Sin(alpha)) + panel4.Width / 2, (float)((-a) * Math.Cos(alpha) + a * Math.Sin(alpha)) +  panel4.Height / 2)
+                };
+                    gImg.FillPolygon(color, rect);
+                }
+
+                g.DrawImage(btm, img);
+                alpha += 0.01;
+                frameCount++;
+            }
+        }
+
+        private void buttonStart1_Click(object sender, EventArgs e)
+        {
+            if (isRunning[0] == false)
+            {
+                threads[0].Resume();
+                isRunning[0] = true;
+            }
+            buttonStop1.Enabled = true;
+            buttonStart1.Enabled = false;
+        }
+
+        private void buttonStop1_Click(object sender, EventArgs e)
+        {
+            if (isRunning[0])
+            {
+                threads[0].Suspend();
+                isRunning[0] = false;
+            }
+            buttonStart1.Enabled = true;
+            buttonStop1.Enabled = false;
+
+        }
+
+        public void Panel2Draw()
+        {
+            Bitmap btm = new Bitmap(panel2.Width, panel2.Height);
+            Graphics gImg = Graphics.FromImage(btm);
+            Graphics g = panel2.CreateGraphics();
+            PointF img = new PointF(0, 0);
+            Random rand = new Random();
+            Pen pen = new Pen(Color.FromArgb(128, 128, 255), 5F);
+
+            double X, Y, dX, dY;
+            int radius = 25;
+            X = rand.Next(radius, panel2.Width - radius);
+            Y = rand.Next(radius, panel2.Height - radius);
+            dX = (rand.Next(1, 5)) * 0.5;
+            dY = (rand.Next(1, 5)) * 0.5;
+
+            while (true)
+            {
+                gImg.Clear(panel2.BackColor);
+                gImg.DrawEllipse(pen, (float)X - radius, (float)Y - radius, radius * 2, radius * 2);
+                gImg.FillEllipse(new SolidBrush(Color.FromArgb(57, 172, 115)), (float)X - radius, (float)Y - radius, radius * 2, radius * 2);
+                IsInRect(ref X, ref Y, ref dX, ref dY, radius, panel2.Width, panel2.Height);
+                g.DrawImage(btm, img);
+            }
+           
+        }
+
+        public bool IsInRect(ref double X, ref double Y, ref double dX, ref double dY, int radius, int width, int height)
+        {
+            if (X <= radius && dX < 0)
+            {
+                dX = -dX;
+                X += dX;
+                Y += dY;
+                return false;
+            }
+            else if (X >= width - radius && dX > 0)
+            {
+                dX = -dX;
+                X += dX;
+                Y += dY;
+                return false;
+            }
+            else if (Y <= radius && dY < 0)
+            {
+                dY = -dY;
+                X += dX;
+                Y += dY;
+                return false;
+            }
+            else if (Y >= height - radius && dY > 0)
+            {
+                dY = -dY;
+                X += dX;
+                Y += dY;
+                return false;
+            }
+            else
+            {
+                X += dX;
+                Y += dY;
+                return true;
+            }
+        }
+
+        private void buttonStart2_Click(object sender, EventArgs e)
+        {
+            if (isRunning[1] == false)
+            {
+                threads[1].Resume();
+                isRunning[1] = true;
+            }
+            buttonStop2.Enabled = true;
+            buttonStart2.Enabled = false;
+        }
+
+        private void buttonStop2_Click(object sender, EventArgs e)
+        {
+            if (isRunning[1] == true)
+            {
+                threads[1].Suspend();
+                isRunning[1] = false;
+            }
+            buttonStart2.Enabled = true;
+            buttonStop2.Enabled = false;
+        }
+
+        public void Panel3Draw()
+        {
+            Graphics g = panel3.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 6F);
+
+            float x1 = 0, y1 = 0, x = 0;
+            float y2;
+            float yEx = 130;
+            float eF = 20;
+
+            g.Clear(panel3.BackColor);
+
+            while (true)
+            {
+                y2 = (float)Math.Sin(x);
+                g.DrawLine(pen, x1 * eF, y1 * eF + yEx, x * eF, y2 * eF + yEx);
+
+                x1 = x;
+                y1 = y2;
+                x += 0.2f;
+
+                if (x * eF >= 350)
+                {
+                    x = x1 = y1 = 0;
+                    g.Clear(panel3.BackColor);
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void buttonStart3_Click(object sender, EventArgs e)
+        {
+            if (isRunning[2] == false)
+            {
+                threads[2].Resume();
+                isRunning[2] = true;
+            }
+            buttonStop3.Enabled = true;
+            buttonStart3.Enabled = false;
+        }
+
+        private void buttonStop3_Click(object sender, EventArgs e)
+        {
+            if (isRunning[2])
+            {
+                threads[2].Suspend();
+                isRunning[2] = false;
+            }
+            buttonStart3.Enabled = true;
+            buttonStop3.Enabled = false;
+        }
+
+
+        public void Panel4Draw()
+        {
+            Graphics g = panel4.CreateGraphics();
+            Bitmap btm = new Bitmap(panel4.Width, panel4.Height);
+            Graphics gImg = Graphics.FromImage(btm);
+            Rectangle area = new Rectangle(0, 10, 70, 35);
+            Pen pen = new Pen(Color.FromArgb(255, 92, 51), 3F);
+            PointF img = new PointF(0, 0);
+
+            while (true)
+            {
+                if (area.X + 5 > panel4.Width)
+                {
+                    area.X = 0;
+                    area.Y += 35;
+                    if (area.Y + 35 > panel4.Height)
+                    {
+                        area.Y = 0;
+                    }
+                }
+
+                gImg.Clear(panel4.BackColor);
+                gImg.DrawRectangle(pen, area);
+                gImg.FillRectangle(new SolidBrush(Color.FromArgb(51, 102, 255)), area);
+                g.DrawImage(btm, img);
+
+                area.X += 5;
+
+                Thread.Sleep(5);
+            }
+        }
+
+        private void buttonStart4_Click(object sender, EventArgs e)
+        {
+            if (isRunning[3] == false)
+            {
+                threads[3].Resume();
+                isRunning[3] = true;
+            }
+            buttonStop4.Enabled = true;
+            buttonStart4.Enabled = false;
+        }
+
+        private void buttonStop4_Click(object sender, EventArgs e)
+        {
+            if (isRunning[3])
+            {
+                threads[3].Suspend();
+                isRunning[3] = false;
+            }
+            buttonStart4.Enabled = true;
+            buttonStop4.Enabled = false;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                if (!isRunning[i])
+                {
+                    threads[i].Resume();
+                    isRunning[i] = true;
+                }
+            }
+            threads.ForEach(p => p.Abort());
+        }
+    }
+}
